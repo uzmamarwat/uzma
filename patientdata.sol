0f21bd9c9f02107cdd79c4d1289ceec0552bd476
uzmamarwat <168737552+uzmamarwat@users.noreply.github.com>
2024-05-13 15:35:07 +0500

    Create patientdata.sol

---
+++ patientdata.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract PatientData {
+    struct Patient {
+        string name;
+        string disease;
+    }
+
+    mapping(address => Patient) public patients;
+
+    event PatientRegistered(address indexed patientAddress, string name, string disease);
+
+    function registerPatient() public {
+        string memory name = "Nida Khan";
+        string memory disease = "Flu";
+        patients[msg.sender] = Patient(name, disease);
+        emit PatientRegistered(msg.sender, name, disease);
+    }
+}
